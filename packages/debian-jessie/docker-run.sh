778595c13c53492700b0689774e998e3e29b2968
Tomas Nozicka <tomas.nozicka@firma.seznam.cz>
2015-09-16 15:36:28 +0200

    Allow setting CONCURRENCY (#3)

--- packages/debian-jessie/docker-run.sh
+++ packages/debian-jessie/docker-run.sh
@@ -3,11 +3,11 @@ set -e
 
 cp -r /dbuild/sources /dbuild/building
 cd /dbuild/building
-make -j clean
+make -j${CONCURRENCY} clean
 
-make -j package-debian-jessie-build-install-dependencies
+make -j${CONCURRENCY} package-debian-jessie-build-install-dependencies
 
-make -j package-debian-jessie-build
+make -j${CONCURRENCY} package-debian-jessie-build
 chmod 644 packages/debian-jessie/*.deb
 cp packages/debian-jessie/*.deb /dbuild/sources/packages/debian-jessie/
 
