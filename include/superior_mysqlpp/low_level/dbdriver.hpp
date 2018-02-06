28432bad581d7e1601377ac6486b6d15db1f131f
Peter Opatril <petr.opatril@firma.seznam.cz>
2018-02-06 11:22:45 +0000

    Remove auto return type on void returning function
    Function sendLongData was misleading, as it implied return of some form of error code.

--- include/superior_mysqlpp/low_level/dbdriver.hpp
+++ include/superior_mysqlpp/low_level/dbdriver.hpp
@@ -1450,9 +1450,9 @@ namespace SuperiorMySqlpp { namespace LowLevel
                 }
             }
 
-            auto sendLongData(unsigned int paramNumber, const std::string& data)
+            void sendLongData(unsigned int paramNumber, const std::string& data)
             {
-                return sendLongData(paramNumber, data.c_str(), data.length());
+                sendLongData(paramNumber, data.c_str(), data.length());
             }
 
             std::string sqlState()
