3b1b13d06402ef5bed32e8e11a2aaa044f65554e
Radek Smejdir <radek.smejdir@firma.seznam.cz>
2019-03-13 11:41:51 +0100

    refactor: using long-opts in build scripts
    makefile + Dockerfiles + docker_run.sh

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 set -e
-PREFIX="`hostname`-`id -u`-`echo $$`-"
+PREFIX="`hostname`-`id --user`-`echo $$`-"
 IMAGE_NAME="${PREFIX}superiormysqlpp-test-mysql"
 CONTAINER_NAME="${PREFIX}superiormysqlpp-testdb"
-docker build --pull -t ${IMAGE_NAME} ../db
-docker rm -f ${CONTAINER_NAME} >/dev/null 2>&1 || true
-docker run -d -P --name ${CONTAINER_NAME} ${IMAGE_NAME} #1>/dev/null
+docker build --pull --tag=${IMAGE_NAME} ../db
+docker rm --force ${CONTAINER_NAME} >/dev/null 2>&1 || true
+docker run --detach --publish-all --name ${CONTAINER_NAME} ${IMAGE_NAME} #1>/dev/null
 MYSQL_HOST=`docker inspect --format='{{.NetworkSettings.IPAddress}}' ${CONTAINER_NAME}`
 set +e
 
@@ -20,13 +20,13 @@ socat "UNIX-LISTEN:$LOCAL_TMP_SOCKET,fork,reuseaddr,unlink-early,mode=777" \
 RESULT=$?
 
 ## TODO Add option to run test in manual mode.
-## It's needed to run docker in interactive mode to get `read` command working, just add `-i` option to docker run in main project's makefile.
+## It's needed to run docker in interactive mode to get `read` command working, just add `--interactive` option to docker run in main project's makefile.
 #echo
 #echo "To connect to test DB:
-#echo "  mysql -uroot -ppassword -h $MYSQL_HOST -P 3306"
+#echo "  mysql --user=root --password=password --host=$MYSQL_HOST --port=3306"
 #echo "To manually test socket forwarding through 3307, try:"
-#echo "  mysql -uroot -ppassword -h $MYSQL_HOST -P 3307"
-#echo "  mysql -uroot -ppassword --socket $LOCAL_TMP_SOCKET"
+#echo "  mysql --user=root --password=password --host=$MYSQL_HOST --port=3307"
+#echo "  mysql --user=root --password=password --socket=$LOCAL_TMP_SOCKET"
 #echo "To run some test case again:"
 #echo "  ./tester ${MYSQL_HOST} 3306 ${CONTAINER_NAME} ${LOCAL_TMP_SOCKET} --reporter=spec --only=<test-name-substring>"
 #echo
@@ -37,7 +37,7 @@ RESULT=$?
 kill $!
 
 echo "Removing temporary created docker container ..."
-docker rm -f ${CONTAINER_NAME}
+docker rm --force ${CONTAINER_NAME}
 
 if [ ! "$?" -eq "0" ]; then
     echo >&2 "Removing docker container '${CONTAINER_NAME}' failed"
